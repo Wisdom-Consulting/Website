96bb7241ee8282d15c775c888d8d39174b24a0c6
sidatii <sidatnouhi@gmail.com>
2023-03-31 03:21:53 +0000

    category seed error

--- Back-end/database/seeders/DatabaseSeeder.php
+++ Back-end/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,14 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Answer;
+use App\Models\Category;
+use App\Models\Chat;
+use App\Models\ChatParticipant;
+use App\Models\Content;
+use App\Models\Message;
+use App\Models\Question;
+use App\Models\Quiz;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -13,7 +21,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         User::factory(5)->create();
+         User::factory(10)->create();
+         Chat::factory(10)->create();
+         Content::factory(10)->create();
+         Question::factory(10)->create();
+         Quiz::factory(10)->create();
+         Message::factory(10)->create();
+         Category::factory(3)->create();
+         Answer::factory(10)->create();
+         ChatParticipant::factory(10)->create();
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
