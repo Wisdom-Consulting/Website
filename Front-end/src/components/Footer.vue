853c42a029dce2fe32757a6df4d0ed4c8ea5c9b9
sidatii <sidatnouhi@gmail.com>
2023-04-04 02:25:59 +0000

    footer done

--- Front-end/src/components/Footer.vue
+++ Front-end/src/components/Footer.vue
@@ -1,14 +1,14 @@
 <script setup>
-import { RouterLink } from 'vue-router'
-import { useAuthStore } from '@/stores/Auth'
-</script>
+import {RouterLink} from 'vue-router'</script>
 
 <template>
-  <div class="flex bg-[#003333] w-full p-4 mt-10 justify-between align-center gap-2 text-xs md:text-lg">
-    <img v-bind:src="'./src/assets/footer_logo.png'" class="md:h-full h-7 self-center" alt="">
-    <RouterLink to="" class="text-white font-bold self-center">History</RouterLink>
-    <RouterLink to="" class="text-white font-bold self-center">Academy</RouterLink>
-    <RouterLink to="" class="text-white font-bold self-center">Contact</RouterLink>
-    <RouterLink to="" class="text-white font-bold self-center">Privacy policy</RouterLink>
+  <div class="flex bg-[#003333] w-full p-4 mt-10 justify-around align-center gap-2 text-xs md:text-lg">
+    <img v-bind:src="'./src/assets/footer_logo.png'" class="md:h-10 h-7 self-center" alt="">
+    <div class="flex justify-around align-center gap-6 text-xs md:text-lg">
+      <RouterLink to="" class="text-white self-center">History</RouterLink>
+      <RouterLink to="" class="text-white self-center">Academy</RouterLink>
+      <RouterLink to="" class="text-white self-center">Contact</RouterLink>
+      <RouterLink to="" class="text-white self-center">Privacy policy</RouterLink>
+    </div>
   </div>
 </template>
